364afba01b61cccccc212d3728fd50f2bd353806
Onderito <ulas.onder@outlook.fr>
2025-06-05 17:57:14 -0400

    add new folder section with different section

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+
+const navIcons = [
+  { src: "/home.svg", alt: "Home icon" },
+  { src: "/bag.svg", alt: "Bag icon" },
+  { src: "/folder.svg", alt: "Folder icon" },
+  { src: "/user.svg", alt: "User icon" },
+];
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="lg:hidden ">
+        <nav className="inset-shadow-2xs rounded-lg fixed bottom-0 p-4 w-full ">
+          <ul className="flex gap-4 items-center justify-evenly">
+            {navIcons.map((icon, index) => (
+              <li key={index}>
+                <Image src={icon.src} alt={icon.alt} width={24} height={24} />
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+
+      <div className="hidden lg:block">
+        <nav className="font-crimson shadow-md p-5 xl:p-8 rounded-2xl fixed right-[-1%] top-14">
+          <ul className="flex items-center gap-10 text-2xl xl:text-3xl">
+            <li>Home</li>
+            <li>About me</li>
+            <li>My experiences</li>
+            <li>My projects</li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/section/profile.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Profile() {
+  return (
+    <div>
+      <Image
+        src="/little-noise.svg"
+        alt="Profile picture"
+        width={300}
+        height={152}
+        className="rounded-full absolute right-[-10%] top-[-2%]"
+      />
+      <section className="p-4 pt-20">
+        <Image
+          src="/profile.svg"
+          alt="Profile picture"
+          width={150}
+          height={142}
+          className=""
+        />
+        <div>
+          <h1 className="font-crimson text-[30px] mt-4">
+            Hey, I'm Ulas Önder.
+          </h1>
+          <h2 className="font-crimson text-[30px]">
+            Developer fullstack &{" "}
+            <span className="font-fredoka font-bold shadow-md p-2 text-[30px] rounded-2xl bg-gradient-to-r from-[#FFA585] to-[#FFEDA0] bg-clip-text text-transparent border-2 border-double border-[#FFEDA0]">
+              Ux/Ui
+            </span>{" "}
+            designer
+          </h2>
+          <p className="text-[16px] mt-2 text-neutral-700 max-w-md">
+            I code like a dev and think like a designer. Always curious, always
+            building.
+          </p>
+          <div className="mt-6 font-crimson text-[22px] flex gap-3">
+            <button className="text-white shadow-md inset-shadow-md bg-[#2F2F2F] rounded-lg p-3">
+              See my projects
+            </button>
+            <button className="text-[#0B66C2] bg-[#0B66C2]/20 rounded-full p-3 ">
+              Linkedin
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
