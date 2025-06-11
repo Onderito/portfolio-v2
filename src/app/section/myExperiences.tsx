26454d4fc20a50d8afdebff1e3e2541e1964732d
Onderito <ulas.onder@outlook.fr>
2025-06-11 16:24:26 -0400

    fix problem

--- src/app/section/myExperiences.tsx
+++ src/app/section/myExperiences.tsx
@@ -4,7 +4,7 @@ export default function MyExperiences() {
     <section className="p-4 mt-10">
       <h2 className="text-[32px] font-crimson">My experiences</h2>
       <div className="mt-8 flex flex-col gap-6">
-        <p className="text-[24px] font-crimson">
+        <h3 className="text-[24px] font-crimson">
           Front end developer at{" "}
           <span className="font-fredoka font-semibold rounded-xl bg-[#0B66C2]/20 p-1 px-2 text-[28px] text-[#50A7FF] inline-block -rotate-4 ">
             DevCSI
@@ -12,27 +12,25 @@ export default function MyExperiences() {
           <p className="text-[#B5B5B5] text-[20px] italic">
             2023 - 2024 (6 months){" "}
           </p>
-        </p>
-        <p className="text-[26px] font-crimson">
+        </h3>
+        <h3 className="text-[26px] font-crimson">
           Full stack developer at{" "}
           <span className="font-fredoka font-semibold rounded-xl bg-[#FF946D]/20 p-1 px-2  text-[28px] text-[#FF946E] inline-block -rotate-4">
             Sealocker
           </span>
-          <br />
           <p className="text-[#B5B5B5] text-[20px] italic">
             2023 - 2024 (6 months){" "}
           </p>
-        </p>
-        <p className="text-[26px] font-crimson">
+        </h3>
+        <h3 className="text-[26px] font-crimson">
           Front end developer at{" "}
           <span className="font-fredoka font-semibold rounded-xl bg-[#0B66C2]/20 p-1 px-2  text-[28px] text-[#50A7FF] inline-block -rotate-4">
             Ubby
           </span>
-          <br />
           <p className="text-[#B5B5B5] italic text-[20px]">
             2024 - 2025 (freelance)
           </p>
-        </p>
+        </h3>
       </div>
     </section>
   );
